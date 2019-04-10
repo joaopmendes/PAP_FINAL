c1b64e2dbce384fa77c18362d6594d126f52f921
João Mendes <joaopedrolopesmendes@gmail.com>
2019-04-10 20:00:59 +0100

    show page done

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -12,19 +12,37 @@
 
     <h1 id=post_title class="display-3 text-center">{{ $post->title }}</h1>
 
-    <div class="container">
+    <div class="container-fluid">
 
       <p id=post_text>
         {{ $post->message }}
 
       </p>
-      <div id="comment-section">
+      <div id="comment-section" class="margin-bottom" style="margin-bottom:3em">
+        <form class='margin-top' action="{{ route('comment_post') }}" method="post">
+            @csrf
+                <div class="card text-left">
+                  <div style="height: 4em">
+                        <input type="text"  name="comment" id="comment_input" class="" placeholder="Escreva um comentário" aria-describedby="helpId">
+                        <input type="hidden" name='blog_id' id='blog_id' value={{ $post->id }}>
+                  </div>
+                </div>
+
+
+        </form>
 
         @foreach ($post->comments as $comment)
-          <div id=comment>
+        <div id=comment class="">
 
-            <h6>{{ $comment->user->name }} <small>{{ $comment->created_at->diffForHumans() }}</small></h6>
-            <p>{{ $comment->comment }}</p>
+            <div class="card text-left">
+              <div class="card-body">
+                    <h6 class="card-title">{{ $comment->user->name }} <small>{{ $comment->created_at->diffForHumans() }}</small></h6>
+                    <hr>
+                    <p id=comment-text class="card-text">{{ $comment->comment }}</p>
+
+              </div>
+            </div>
+
           </div>
 
         @endforeach

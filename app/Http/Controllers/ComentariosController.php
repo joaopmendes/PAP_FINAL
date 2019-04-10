ed387156b9bda068cdc7fa34c4cb81cddece2479
João Mendes <joaopedrolopesmendes@gmail.com>
2019-04-10 20:00:11 +0100

    create method added

--- app/Http/Controllers/ComentariosController.php
+++ app/Http/Controllers/ComentariosController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class ComentariosController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function create(Request $req){
+        $req->validate([
+                'blog_id'=>'required',
+                'comment'=>'required'
+        ]);
+
+        $comment = new \App\Comment();
+        $comment->blog_id = $req->blog_id;
+        $comment->user_id = \Auth::user()->id;
+        $comment->comment = $req->comment;
+        $comment->save();
+
+        return redirect()->route('blog.show', ['blog'=>$req->blog_id]);
+
+
+    }
 }

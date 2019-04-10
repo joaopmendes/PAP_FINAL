9f2691b48658360cfa29b0a717ea15430dcd06fa
João Mendes <joaopedrolopesmendes@gmail.com>
2019-04-10 17:23:44 +0100

    Added more text to the blog message (seeding)

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Blogpost::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(),
-        'message' => $faker->paragraph(),
+        'message' => $faker->paragraph() . $faker->paragraph() . $faker->paragraph() . $faker->paragraph() . $faker->paragraph() . $faker->paragraph() . $faker->paragraph(),
     ];
 });

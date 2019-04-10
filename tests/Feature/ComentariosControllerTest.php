34762a376d7d256d5ed361e167166700d17652a5
João Mendes <joaopedrolopesmendes@gmail.com>
2019-04-10 17:29:03 +0100

    News (in blanc)

---
+++ tests/Feature/ComentariosControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ComentariosControllerTest extends TestCase
+{
+  use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+
+    }
+}

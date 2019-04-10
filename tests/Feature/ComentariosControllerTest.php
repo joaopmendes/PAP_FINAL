458f847b8799012bff7a2b738b1f4dfd5eb51851
João Mendes <joaopedrolopesmendes@gmail.com>
2019-04-10 20:02:06 +0100

    added test_if_comentaros_are_being_created

--- tests/Feature/ComentariosControllerTest.php
+++ tests/Feature/ComentariosControllerTest.php
@@ -8,14 +8,33 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ComentariosControllerTest extends TestCase
 {
-  use RefreshDatabase;
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testExample()
+    public function test_if_comentarios_are_being_created()
     {
+        $table='comments';
+        $comment_test = 'test comment';
+        $blog = factory(\App\Blogpost::class)->create();
+        $user = factory(\App\User::class)->create();
+
+        $this->actingAs($user)->post(route('comment_post'), [
+            'comment'=>$comment_test,
+            'blog_id'=>$blog->id
+        ]);
+        $this->assertDatabaseHas($table, [
+            'blog_id'=>$blog->id,
+            'user_id'=>$user->id,
+            'comment'=>$comment_test
+        ]);
+
+
+
+
 
     }
 }

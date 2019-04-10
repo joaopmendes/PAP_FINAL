c975500619f8a8e9c5a269e9ea8dafbac7289f2f
João Mendes <joaopedrolopesmendes@gmail.com>
2019-04-10 17:24:10 +0100

    added some css to show page

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -8,19 +8,28 @@
 @endsection
 @section('content')
 
+  <div id="blog_show">
+
+    <h1 id=post_title class="display-3 text-center">{{ $post->title }}</h1>
+
     <div class="container">
-        <h1>{{ $post->title }}</h1>
-        <p >
-            {{ $post->message }}
 
-        </p>
-        <div class="comment-section">
-            <hr>
-            @foreach ($post->comments as $comment)
-                <h4>{{ $comment->user->name }}</h4>
-                    {{ $comment->comment }}
-            @endforeach
+      <p id=post_text>
+        {{ $post->message }}
 
-        </div>
+      </p>
+      <div id="comment-section">
+
+        @foreach ($post->comments as $comment)
+          <div id=comment>
+
+            <h6>{{ $comment->user->name }} <small>{{ $comment->created_at->diffForHumans() }}</small></h6>
+            <p>{{ $comment->comment }}</p>
+          </div>
+
+        @endforeach
+
+      </div>
     </div>
+  </div>
 @endsection

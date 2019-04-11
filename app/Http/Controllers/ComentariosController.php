d1cf497521792f857e1a9a37a80a16d72645b539
João Mendes <joaopedrolopesmendes@gmail.com>
2019-04-11 17:43:08 +0100

    Added delete function

--- app/Http/Controllers/ComentariosController.php
+++ app/Http/Controllers/ComentariosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\blogpost;
 
 class ComentariosController extends Controller
 {
@@ -25,5 +26,15 @@ class ComentariosController extends Controller
         return redirect()->route('blog.show', ['blog'=>$req->blog_id]);
 
 
+    }
+    public function delete($comment_id){
+
+        $comment = \App\Comment::findOrFail($comment_id);
+
+        if ((\Auth::user()->id == $comment->user->id) || (\Auth::user()->admin == 'true')){
+            $comment->delete();
+            return back();
+        }
+        return redirect(route('home'));
     }
 }
